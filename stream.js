64f170e0d58aa488f9f2c487e139475b1e93cb31
Nirosha55 <niroshaniru323@gmail.com>
2024-01-28 20:43:05 +0530

    firstchange

---
+++ stream.js
@@ -0,0 +1,15 @@
+const fs =require('fs');
+
+const server=require('http').createServer();
+
+
+server.on('request',(req,res) => {
+    fs.readFile("test-file.txt", (err, data) => {
+        if (err) console.log(err);
+        res.end(data);
+       });
+});
+
+server.listen(8000,'127.0.0.1',() => {
+    console.log("Listening........8000 port");
+});
