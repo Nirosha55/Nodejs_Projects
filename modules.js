135f7e0e7cd1c9f476aa679a1c92637d12e78ea4
Nirosha55 <niroshaniru323@gmail.com>
2024-01-29 15:13:23 +0530

    added origins

--- modules.js
+++ modules.js
@@ -1,4 +1,7 @@
 const C = require("./test-module");
 
-const cal = new C();
-console.log(cal.mod(20,10));
+// const cal = new C();
+// console.log(cal.mod(20,10));
+//  or
+const {add,multiply,diviion,mod} =require('./test-module');
+console.log(add(20,10));
